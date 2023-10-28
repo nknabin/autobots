47bd27ba84d3b1053b21fab0d16cd33f88f44c80
Nabin Koirala <nabin@asus>
2023-10-28 17:03:42 +0545

    Fix rbenv plugins path and change go version

--- install-tools.sh
+++ install-tools.sh
@@ -4,7 +4,7 @@ echo "running setup-env.."
 source ./setup-env.sh
 
 # install go if doesn't exist
-if [[ -z "$GOPATH" ]]; then
+if [[ -z "$GO_HOME" ]]; then
 	echo "go not found.."
 
 	PS3="Install go now : "
@@ -15,11 +15,11 @@ if [[ -z "$GOPATH" ]]; then
 		yes)
 			echo "Installing go now..."
 
-			wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
+			wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
 
 			# remove old go if exist
 			sudo rm -rf /usr/local/go
-			sudo tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
+			sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
 
 			echo "export GOROOT=/usr/local/go" >> ~/.bashrc
 			echo "export GOPATH=$HOME/go" >> ~/.bashrc
@@ -47,16 +47,23 @@ fi
 # install ruby with rbenv
 echo "Installing rbenv and ruby.."
 if [[ ! -d $HOME/.rbenv ]]; then
-	git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-	echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >>~/.bashrc
+	git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
+	mkdir -p ~/.rbenv/plugins
+	git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+	echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+
 	sleep 5
-	source $HOME/.bashrc
-	git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-	#TODO install ruby with rbenv
+
+	. ~/.bashrc
+	rbenv install $(rbenv install -l|head -n 1)
 fi
 
 # essentials
-sudo $package_manager_install jq nmap tor
+sudo $package_manager_install jq nmap tor whois
+#TODO dig, nslookup, whois
+
+# dependencies
+sudo apt install -y libpcap-dev
 
 # utils
 go install github.com/tomnomnom/unfurl@latest
@@ -71,7 +78,9 @@ go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 
 go install github.com/rverton/webanalyze/cmd/webanalyze@latest
-cd $HOME && wget https://raw.githubusercontent.com/rverton/webanalyze/master/technologies.json
+if [ ! -f ~/technologies.json ]; then
+	cd $HOME && wget https://raw.githubusercontent.com/rverton/webanalyze/master/technologies.json
+fi
 
 go install github.com/tomnomnom/waybackurls@latest
 
